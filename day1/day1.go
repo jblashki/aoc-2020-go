b145db7e00ad4118f64cc7b94bf44dd510c44625
Jonathan Blashki <jblashki@gmail.com>
2020-12-02 09:38:17 +1100

    Added day1

--- day1/day1.go
+++ day1/day1.go
@@ -3,9 +3,13 @@ package day1
 import (
 	"errors"
 	"fmt"
+
+	"github.com/jblashki/aoc-filereader-go"
 )
 
 const name = "Day 1"
+
+const input_file = "./day1/numbers"
 
 func RunDay(verbose bool) {
 	var aResult int
@@ -20,21 +24,55 @@ func RunDay(verbose bool) {
 	if err != nil {
 		fmt.Printf("%va: **** Error: %q ****\n", name, err)
 	} else {
-		fmt.Printf("%va: Program Result = %v\n", name, aResult)
+		fmt.Printf("%va: Result = %v\n", name, aResult)
 	}
 
 	bResult, err = b()
 	if err != nil {
 		fmt.Printf("%vb: **** Error: %q ****\n", name, err)
 	} else {
-		fmt.Printf("%vb: Program Result = %v\n", name, bResult)
+		fmt.Printf("%vb: Result = %v\n", name, bResult)
 	}
 }
 
 func a() (int, error) {
-	return 0, errors.New("Not Complete Yet")
+	numbers, err := filereader.ReadAllInts(input_file)
+	if err != nil {
+		errormsg := fmt.Sprintf("Failed to read from #q: %v", input_file, err)
+		return 0, errors.New(errormsg)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		numA := numbers[i]
+		for j := i + 1; j < len(numbers); j++ {
+			numB := numbers[j]
+
+			if numB+numA == 2020 {
+				return numA * numB, nil
+			}
+		}
+	}
+	return 0, errors.New("Couldn't find numbers")
 }
 
 func b() (int, error) {
-	return 0, errors.New("Not Complete Yet")
+	numbers, err := filereader.ReadAllInts(input_file)
+	if err != nil {
+		errormsg := fmt.Sprintf("Failed to read from #q: %v", input_file, err)
+		return 0, errors.New(errormsg)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		numA := numbers[i]
+		for j := i + 1; j < len(numbers); j++ {
+			numB := numbers[j]
+			for k := j + 1; k < len(numbers); k++ {
+				numC := numbers[k]
+				if numB+numA+numC == 2020 {
+					return numA * numB * numC, nil
+				}
+			}
+		}
+	}
+	return 0, errors.New("Couldn't find numbers")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module aoc2020
 
 go 1.14
+
+require github.com/jblashki/aoc-filereader-go v1.1.0
