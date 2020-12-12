3ae7bc114b4d23340cd81891b12df10223bc1172
Jonathan Blashki <jblashki@gmail.com>
2020-12-12 17:54:41 +1100

    Added day 12

--- day12/day12.go
+++ day12/day12.go
@@ -1,12 +1,18 @@
 package day12
 
 import (
-	"errors"
 	"fmt"
+	"math"
+	"math/cmplx"
+	"strconv"
+
+	filereader "github.com/jblashki/aoc-filereader-go"
 )
 
 const name = "Day 12"
+const inputFile = "./day12/instructions"
 
+// RunDay runs Advent of Code Day 12 Puzzle
 func RunDay(verbose bool) {
 	var aResult int
 	var bResult int
@@ -31,10 +37,111 @@ func RunDay(verbose bool) {
 	}
 }
 
+type op int
+
+const (
+	opNorth   op = 'N'
+	opSouth   op = 'S'
+	opEast    op = 'E'
+	opWest    op = 'W'
+	opLeft    op = 'L'
+	opRight   op = 'R'
+	opForward op = 'F'
+)
+
+func (o op) String() string {
+	switch o {
+	case opNorth:
+		return "North"
+	case opSouth:
+		return "South"
+	case opEast:
+		return "East"
+	case opWest:
+		return "West"
+	case opLeft:
+		return "Left"
+	case opRight:
+		return "Right"
+	case opForward:
+		return "Forward"
+	}
+	return ""
+}
+
 func a() (int, error) {
-	return 0, errors.New("Not Complete Yet")
+	answer, err := calcDistance(false)
+
+	return answer, err
 }
 
 func b() (int, error) {
-	return 0, errors.New("Not Complete Yet")
+	answer, err := calcDistance(true)
+
+	return answer, err
+}
+
+func calcDistance(partB bool) (int, error) {
+	lines, err := filereader.ReadLines(inputFile)
+	if err != nil {
+		return 0, err
+	}
+
+	pos := complex(0, 0)
+
+	way := complex(1, 0)
+	if partB {
+		way = complex(10, 1)
+	}
+
+	for _, line := range lines {
+		op := op(line[0])
+		num, _ := strconv.Atoi(line[1:])
+
+		switch op {
+		case opNorth:
+			if !partB {
+				pos += complex(float64(0), float64(num))
+			}
+			way += complex(float64(0), float64(num))
+
+		case opSouth:
+			if !partB {
+				pos -= complex(float64(0), float64(num))
+			}
+			way -= complex(float64(0), float64(num))
+
+		case opEast:
+			if !partB {
+				pos += complex(float64(num), float64(0))
+			}
+			way += complex(float64(num), float64(0))
+
+		case opWest:
+			if !partB {
+				pos -= complex(float64(num), float64(0))
+			}
+			way -= complex(float64(num), float64(0))
+
+		case opLeft:
+			rotations := num / 90
+			diff := (way - pos) * cmplx.Pow(0+1i, complex(float64(rotations), 0))
+			way = pos + diff
+
+		case opRight:
+			rotations := num / 90
+			diff := (way - pos) * cmplx.Pow(0-1i, complex(float64(rotations), 0))
+			way = pos + diff
+
+		case opForward:
+			diff := (way - pos)
+			pos += diff * complex(float64(num), 0)
+			way = pos + diff
+		}
+	}
+
+	answer := math.Abs(real(pos)) + math.Abs(imag(pos))
+	answerInt := int(answer)
+
+	return answerInt, nil
 }
